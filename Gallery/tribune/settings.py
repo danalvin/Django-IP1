84bfdce5ea19bfa8a808e2ee9acfe7656efeca52
danalvin <mungaiwaituika@gmail.com>
2018-09-30 18:47:57 +0300

    trial #5 to Heroku

--- Gallery/tribune/settings.py
+++ Gallery/tribune/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-PROJECT_DIR PROJECT_D = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_DIR PROJECT_D = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
