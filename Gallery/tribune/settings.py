ec07e96bcc036d3045b4828e7de8a3ed5d2b0af3
danalvin <mungaiwaituika@gmail.com>
2018-10-01 07:57:15 +0300

    errors solved in Procfile

--- Gallery/tribune/settings.py
+++ Gallery/tribune/settings.py
@@ -126,9 +126,11 @@ USE_TZ = True
 # PROJECT_DIR PROJECT_D = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/static/
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
